c153263220060197a4cd88275719e994b1b459e0
morski <martengustavsson@gmail.com>
2018-05-12 03:23:32 +0300

    missing files

---
+++ EuroUi/src/Dashboard/ContestantFlag.js
@@ -0,0 +1,20 @@
+
+import React, { Component } from 'react';
+
+
+class ContestantFlag extends Component {
+
+    constructor(props, context) {
+        super(props, context);
+    }
+
+    render() {
+        return (
+            <g transform={`translate(${this.props.x-25},${this.props.y-12})`}>
+                <image xlinkHref={"../images/flags/round/svg/"+this.props.payload.value.toLowerCase().replace(" ", "-")+".svg"} x={0} y={0} height="20px" width="20px" textAnchor="middle" fill="#666" />
+            </g>
+        )
+    }
+}
+
+export default ContestantFlag;
